d0e5b900d16f2c59f4f7b2945220d2e4fa1c670b
berkgurdamar <berk_gurdamar@hotmail.com>
2022-08-12 15:38:00 +0300

    add model description

---
+++ R/data.R
@@ -0,0 +1,12 @@
+#' All atoms used model
+#'
+#' Adaboost model built using 7Å cutoff and all atoms in the structure.
+#'
+"caret_adaboost_all"
+
+
+#' Only Cα atoms used model
+#'
+#' Adaboost model built using 7Å cutoff and only Cα atoms in the structure.
+#'
+"caret_adaboost_ca"
